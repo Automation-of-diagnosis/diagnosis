ce1219589f34f8db7700afc4d6196ed9e869888d
kmoscow <kd200681@mail.ru>
2020-03-24 23:40:27 +0300

    ability to process data from user

--- forms.py
+++ forms.py
@@ -12,13 +12,14 @@ choices_srad = [
 
 
 class LoginForm(FlaskForm):
-    ful_name = StringField('Ф.И.О.', validators=[DataRequired()], render_kw={"class": "form-control"})
-    age = IntegerField('Возраст', validators=[DataRequired()], render_kw={"class": "form-control"})
-    number = IntegerField('Номер больничного листа', validators=[DataRequired()], render_kw={"class": "form-control"})
+    ful_name = StringField('Ф.И.О.', render_kw={"class": "form-control"})
+    age = IntegerField('Возраст',  render_kw={"class": "form-control"})
+    number = IntegerField('Номер больничного листа',  render_kw={"class": "form-control"})
     srad = SelectField('срАД (мм рт.ст.)', choices=choices_srad)
-    creatinine = IntegerField('Креатинин (ммоль/л)', validators=[DataRequired()], render_kw={"class": "form-control"})
-    platelets = IntegerField('Тромбоциты (10*9/л)', validators=[DataRequired()], render_kw={"class": "form-control"})
-    bilirubin = IntegerField('Билирубин (ммоль/л)', validators=[DataRequired()], render_kw={"class": "form-control"})
-    pao2_fio2 = IntegerField('PaO2/FiO2 (мм рт.ст.)', validators=[DataRequired()], render_kw={"class": "form-control"})
-    gsc = IntegerField('GSC', validators=[DataRequired()], render_kw={"class": "form-control"})
+    creatinine = IntegerField('Креатинин (ммоль/л)',  render_kw={"class": "form-control"})
+    platelets = IntegerField('Тромбоциты (10*9/л)',  render_kw={"class": "form-control"})
+    bilirubin = IntegerField('Билирубин (ммоль/л)', render_kw={"class": "form-control"})
+    pao2_fio2 = IntegerField('PaO2/FiO2 (мм рт.ст.)',  render_kw={"class": "form-control"})
+    gsc = IntegerField('GSC', render_kw={"class": "form-control"})
     submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary"})
+# validators=[DataRequired()],
